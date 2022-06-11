190078fa651242df3a6b3c65aad8fd09cae56380
Dinyad <donatien.yeto@gmail.com>
2022-06-11 11:20:29 +0200

    first commit

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express, { json } from 'express';
+import bodyParser from 'body-parser';
+import router from './src/routes/lists.route.js'
+import cors from 'cors'
+import mongoose from 'mongoose';
+
+const app = express()
+
+
+//Modifier pour vous connecter a votre db
+mongoose.connect("mongodb+srv://<username>:<password>@cluster0.1m472.mongodb.net/<dbname>?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true } );
+
+const db = mongoose.connection;
+db.on("error", 
+console.error.bind(console, "connection error: ")); 
+db.once("open", function () { console.log("Connected successfully"); }); 
+
+
+app.use(bodyParser.json())
+app.use(cors())
+app.get("/",(req,res) => {
+    res.send('TP Web')
+})
+app.use(cors())
+app.use('/lists',router)
+
+
+app.listen(8081, () => {
+    console.log("Serveur à l'écoute")
+})
